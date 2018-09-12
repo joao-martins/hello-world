500790820f93a2fa6f5e3e3349aeabbcfed12e96
João Martins <joao.martins@letshare.com>
2018-09-12 14:39:15 +0100

    Run the app on the port defined by the environment variable PORT (or 3000 as fallback)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const routes = require('./routes');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.engine('hbs', handlebars({ extname: 'hbs', defaultLayout: 'main' }));
@@ -9,4 +11,7 @@ app.set('view engine', 'hbs');
 
 app.use(routes);
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
